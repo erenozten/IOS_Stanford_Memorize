1ad33cbbbb57bac9d54f5b9b6381444b31765410
Eren ÖZTEN <erenozten@gmail.com>
2025-01-31 15:01:37 +0300

    add view modifiers in ContentView - https://youtu.be/n1qabtjZ_jg?si=mFamr-JxjrPunUSu&t=3483
    min 58

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(content: {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("🍟").font(.largeTitle)
         })
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
     }
 }
@@ -22,3 +21,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
