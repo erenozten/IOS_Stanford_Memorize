f805abff242fa85ca639b37e57aa094dec715822
Eren ÖZTEN <erenozten@gmail.com>
2025-01-31 13:48:29 +0300

    add "content" parameter to VStack to understand the functional programming concepts easily
     VStack(content: {
                Image(systemName: "globe")
                    .imageScale(.large)
                    .foregroundStyle(.tint)
                Text("Hello, world!")
            })

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(content: {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-        }
+        })
         .padding()
     }
 }
