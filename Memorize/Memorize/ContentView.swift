a5107de2d82ed912d4c346e0e6b7f1b7b3042862
Eren ÖZTEN <erenozten@gmail.com>
2025-01-31 20:17:18 +0300

    add new RoundedRectangle for background color, see https://youtu.be/n1qabtjZ_jg?si=N1do7XjrC7CuNrtC&t=3741

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack(content: {
-            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
+            RoundedRectangle(cornerRadius: 12).fill(Color.blue)
+            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
             
             Text("🍟").font(.largeTitle)
         })
