7b00ffa8c66449e9296887c12067b42d2f0b3864
Eren ÖZTEN <erenozten@gmail.com>
2025-01-31 20:29:11 +0300

    make code better: remove duplicate code, create CardView and use it instead, see https://youtu.be/n1qabtjZ_jg?si=r1IfchSZZVGkA2lh&t=3964

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,36 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack(content: {
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12).fill(Color.blue)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
-                
-                Text("🍟").font(.largeTitle)
-            })
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12).fill(Color.blue)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
-                
-                Text("🍟").font(.largeTitle)
-            })
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12).fill(Color.blue)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
-                
-                Text("🍟").font(.largeTitle)
-            })
-            ZStack(content: {
-                RoundedRectangle(cornerRadius: 12).fill(Color.blue)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
-                
-                Text("🍟").font(.largeTitle)
-            })
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+
         })
         .foregroundColor(.orange)
         .imageScale(.small)
         .padding()
     }
 }
+
+
+
+struct CardView: View{
+    var body: some View{
+        ZStack(content: {
+            RoundedRectangle(cornerRadius: 12).fill(Color.blue)
+            RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 13) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
+            
+            Text("🍟").font(.largeTitle)
+        })
+    }
+}
+
+
+
+
+
 
 #Preview {
     ContentView()
