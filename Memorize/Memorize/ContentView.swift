cf057ac8bbe78256d9643e596e26713697582753
Eren ÖZTEN <erenozten@gmail.com>
2025-02-01 14:24:31 +0300

    create 'local variable' named 'base' =>             var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12) => see https://youtu.be/sXiD-2XrkKQ?si=2c9NOyfS-8WrRaIF&t=880

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -28,17 +28,19 @@ struct CardView: View{
     var body: some View{
         ZStack(content: {
             
+            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12).fill(Color.white)
-                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth:10) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
+                base.fill(Color.white)
+                base.strokeBorder(lineWidth:10) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
                 Text("🍟").font(.largeTitle)
             }
             else{
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
         })
     }
 }
+
 
 
 
