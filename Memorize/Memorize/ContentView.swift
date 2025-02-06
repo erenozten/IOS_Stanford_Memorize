82a0c45f91d47d31b6efe86c179b36e76e0c62aa
Eren ÖZTEN <erenozten@gmail.com>
2025-02-06 18:42:17 +0300

    make .storeBorder better

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -139,7 +139,7 @@ struct CardView: View{
             
             Group {
                 base.fill(Color.white) // arkaplan rengi
-                base.strokeBorder(lineWidth:5) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
+                base.strokeBorder(lineWidth:2) // Sadece çerçeve çizer. Background color vesaire hiçbir şey yok. (background color için bu rectangle'ın hemen arkasına bi rectangle ekleyeceğiz --> RoundedRectangle şekline bir arka plan rengi verebilirsiniz. Ancak, doğrudan .strokeBorder() kullanırken arka plan rengi ekleyemezsiniz, çünkü .strokeBorder() sadece kenarlık çizer. Bunun yerine .background() veya .fill() kullanarak bir arka plan rengi ekleyebilirsiniz.
                 Text(content).font(.largeTitle)
             }
             .opacity(isFaceUp ? 1 : 0) // isFaceUp ise -> Group'takilerin tümünü göster! böylece kart görünür olmuş oluyor
