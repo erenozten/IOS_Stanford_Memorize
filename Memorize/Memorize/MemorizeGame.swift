9ec7c33f27eb8c92fca6f1fc363e04b92b19e065
Eren ÖZTEN <erenozten@gmail.com>
2025-02-07 00:48:39 +0300

    lecture 3) Create MODEL named MemorizeGame - https://youtu.be/W1ymVx6dmvc?si=cWk_P10wdvHWLNGq&t=3673

---
+++ Memorize/Memorize/MemorizeGame.swift
@@ -0,0 +1,22 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Eren Özten on 7.02.2025.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent>{
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        
+    }
+    
+    struct Card{
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
